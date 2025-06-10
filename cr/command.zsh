88ee175438eb7cfa24f2ac7a0d0102381d9fdd12
Dmitry Rubinstein <rubinsteindb@gmail.com>
2025-06-10 09:08:53 +0300

    Fix command substitution

--- cr/command.zsh
+++ cr/command.zsh
@@ -116,7 +116,7 @@ __mlnj_cde_cr_get_aws_url() {
     
     # Check if running in command substitution (suppress stderr if so)
     local in_subshell=false
-    if [[ "$SHLVL" -gt 1 ]] || [[ -n "$ZSH_SUBSHELL" ]] || [[ "$BASH_SUBSHELL" -gt 0 ]]; then
+    if [[ -n "$ZSH_SUBSHELL" ]] || [[ "$BASH_SUBSHELL" -gt 0 ]]; then
         in_subshell=true
     fi
     
